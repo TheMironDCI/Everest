6f2afcc6fb39f2296fb9af2d4c2b321799529630
TheMironDCI <shaxobiddinovmironshoh@gmail.com>
2023-08-08 22:44:04 +0500

    ss

--- src/Everest.js
+++ src/Everest.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Section1/Header'
 import Courses from './Section1/Courses';
-import MultiItemCarousel from './Section1/MultiItemCarousel';
 
 export default function Everest() {
   return (
