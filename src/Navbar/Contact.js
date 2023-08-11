b51c61b55f09bb6c3af4c69b65683ad83ce33cd1
TheMironDCI <shaxobiddinovmironshoh@gmail.com>
2023-08-11 17:02:02 +0500

    lll

--- src/Navbar/Contact.js
+++ src/Navbar/Contact.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Contact.css";
-import { Link } from "react-router-dom";
-
 const Contact = () => {
   return (
     <div className="contact-container container">
